03b01e5c4bd7e10162f97be62af84ab95fd4f248
lzl-2007 <1246765606@qq.com>
2026-04-13 21:40:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ const CONFIG = {
     intro: '工科学生一枚',
     quote: '',
     /** 头像：可把图片放到 assets/ 下，例如 assets/avatar.jpg */
-    avatar: 'assets/秘书处_线条.png',
+    avatar: '781079071ae30f7b76e48c48f4582fe8.jpg',
     avatarAlt: 'LZL',
     aboutTitle: 'About Me',
     /** 右侧「关于我」列表；有 href 时渲染为链接 */
